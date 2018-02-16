d80e5ec188e9034ac45cb4a8a2c1909e20cefc60
toanhtran <toanh.t.tran@gmail.com>
2018-02-15 20:59:56 -0800

    Add checkmark for row

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoListViewController: UITableViewController {
+	
+	let itemArray = ["Find Mike", "Buy Eggos", "Destory Demogrogon"]
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		
+		
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	//MARK - Tableview Datasource Methods
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return itemArray.count
 	}
-
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+		
+		cell.textLabel?.text = itemArray[indexPath.row]
+		
+		return cell
+	}
+	
+	//MARK - TableView Delegate Methods
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		//print(itemArray[indexPath.row])
+		
+		
+		if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark  {
+			tableView.cellForRow(at: indexPath)?.accessoryType = .none
+		} else {
+			tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+		}
+		
+		tableView.deselectRow(at: indexPath, animated: true)
+	}
+	
+	
 
 }
 
