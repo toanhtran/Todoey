29bd0c7ff185cbd6b62a217f3666793b5d44ffb5
toanhtran <toanh.t.tran@gmail.com>
2018-02-22 15:45:46 -0800

    Persisting data using Core Data.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,22 +7,26 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
 	
-	var itemArray = [Item()]
-	let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+	var itemArray = [Item]()
 	
+	var selectedCategory : Category? {
+		didSet{
+			loadItems()
+		}
+	}
 	
-	let defaults = UserDefaults.standard
-
+	let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
+		print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
 		
-		print(dataFilePath)
-		
-		loadItems()
+		loadItems() 
 		
 	}
 	
@@ -51,9 +55,10 @@ class TodoListViewController: UITableViewController {
 	//MARK - TableView Delegate Methods
 	
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		//print(itemArray[indexPath.row])
 		
-	
+//		context.delete(itemArray[indexPath.row])
+//		itemArray.remove(at: indexPath.row)
+		
 		itemArray[indexPath.row].done = !itemArray[indexPath.row].done
 		
 		saveItems()
@@ -72,9 +77,12 @@ class TodoListViewController: UITableViewController {
 		let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
 			//what will happen once the user clicks the Add Item Button on our UIAlert
 			
-			let newItem = Item()
-			newItem.title = textField.text!
 			
+			
+			let newItem = Item(context: self.context)
+			newItem.title = textField.text!
+			newItem.done = false
+			newItem.parentCategory = self.selectedCategory
 			self.itemArray.append(newItem)
 			
 			self.saveItems()
@@ -86,6 +94,7 @@ class TodoListViewController: UITableViewController {
 			textField = alertTextField
 			print(alertTextField)
 		}
+		
 		alert.addAction(action)
 		
 		present(alert, animated: true, completion: nil)
@@ -94,29 +103,63 @@ class TodoListViewController: UITableViewController {
 	//MARK - Model Manipulation Methods
 	
 	func saveItems() {
-		let encoder = PropertyListEncoder()
 		
 		do{
-			let data = try encoder.encode(itemArray)
-			try data.write(to: dataFilePath!)
+			try context.save()
 		} catch {
-			print("Error encoding.\(error)")
+			print("Error saving context.\(error)")
 		}
 		
 		self.tableView.reloadData()
 	}
 	
-	func loadItems() {
-		if let data = try? Data(contentsOf: dataFilePath!) {
-			let decoder = PropertyListDecoder()
-			do {
-				itemArray = try decoder.decode([Item].self, from: data) {
-				} catch {
-					print("Error decdoing item array, \(error)")"
-				}
+	func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil ) {
+		
+		let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+		
+		if let additonalPredicate = predicate {
+			request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additonalPredicate])
+		} else {
+			request.predicate = categoryPredicate
 		}
+		
+		
+		do {
+			itemArray = try context.fetch(request)
+		} catch {
+			print("Error fetching data from context \(error)")
+		}
+		
+		tableView.reloadData()
 	}
-	
 
 }
 
+//MARK: - Search bar methods
+
+extension TodoListViewController: UISearchBarDelegate {
+	
+	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+	
+		let request: NSFetchRequest<Item> = Item.fetchRequest()
+		
+		request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+		
+		request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+		
+		loadItems(with: request)
+		
+	}
+	
+	func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+		if searchBar.text?.count == 0 {
+			loadItems()
+			
+			DispatchQueue.main.async {
+				searchBar.resignFirstResponder()
+			}
+			
+			
+		}
+	}
+}
